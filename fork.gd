e1c1fb048eeb137799444916aeec1612b8a737aa
Panda248 <40081083+Panda248@users.noreply.github.com>
2025-04-11 21:47:56 -0400

    more scenes

--- fork.gd
+++ fork.gd
@@ -1,9 +1,16 @@
 extends Node
 
+@export var computer_scene : PackedScene
+@export var hallway_scene : PackedScene
+@export var window_scene : PackedScene
 
 func _on_hallway_pressed() -> void:
-	pass # Replace with function body.
+	get_tree().change_scene_to_packed(hallway_scene)
 
 
 func _on_computer_pressed() -> void:
-	pass # Replace with function body.
+	get_tree().change_scene_to_packed(computer_scene)
+
+
+func _on_window_pressed() -> void:
+	get_tree().change_scene_to_packed(window_scene)
